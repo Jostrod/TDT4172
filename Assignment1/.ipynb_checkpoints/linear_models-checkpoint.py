a7a45d89b0ea2a953aad1fb90890c47aa8cabca2
Jostein <jostroda@stud.ntnu.no>
2026-09-05 21:34:46 +0200

    Init commit

---
+++ Assignment1/.ipynb_checkpoints/linear_models-checkpoint.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+class LinearRegression():
+    def __init__(self, lr=0.001, n_iterations=1000):
+        self.weights = None
+        self.bias = None
+        
+        self.lr = lr
+        self.n_iterations = n_iterations
+        
+        self.loss_history = []
+        
+    def fit(self, X, y):
+        """
+        Estimates parameters for the classifier
+        
+        Args:
+            X (array<m,n>): a matrix of floats with
+                m rows (#samples) and n columns (#features)
+            y (array<m>): a vector of floats
+        """
+        # ====================================
+        # YOUR CODE GOES HERE
+        # ====================================
+
+        self.weights = np.zeros(X.shape[1])
+        self.bias = 0
+
+
+        for _ in range(self.n_iterations):
+            y_pred = np.dot(X, self.weights) + self.bias # Formel for y-pred
+
+            error = y_pred - y #Error for gradient bias
+
+            m = X.shape[0] # Antall elementer
+
+            grad_bias = np.sum(error)
+
+            # Mean squared error
+            mse = np.square(np.subtract(y, y_pred)).mean()
+
+            #Finn grad w og grad b
+            grad_w = (2/m) * np.dot(X.T, y_pred-y)
+            grad_b = (2/m) * np.sum(y_pred - y)
+
+            #Oppdater
+            self.weights -= self.lr * grad_w
+            self.bias -= self.weights * grad_b
+
+
+            #Lagre loss
+            self.loss_history.append(mse)
+
+
+
+
+
+
+        raise NotImplementedError("LinearRegression.fit is not implemented yet.")
+    
+    def predict(self, X):
+        """
+        Generates predictions
+        
+        Note: should be called after .fit()
+        
+        Args:
+            X (array<m,n>): a matrix of floats with 
+                m rows (#samples) and n columns (#features)
+            
+        Returns:
+            A length m array of floats
+        """
+        # ====================================
+        # YOUR CODE GOES HERE
+        # ====================================
+
+        return np.dot(X, self.weights) + self.bias
+        # raise NotImplementedError("LinearRegression.predict is not implemented yet.")
+    
+class LogisticRegression():
+    def __init__(self, lr=0.001, n_iterations=1000):
+        self.weights = None
+        self.bias = None
+        
+        self.lr = lr
+        self.n_iterations = n_iterations
+        
+        self.loss_history = []
+    
+    def fit(self, X, y):
+        # ====================================
+        # YOUR CODE GOES HERE
+        # ====================================
+        raise NotImplementedError("LogisticRegression.fit is not implemented yet.")
+    
+    def predict_proba(self, X):
+        # ====================================
+        # YOUR CODE GOES HERE
+        # ====================================
+        raise NotImplementedError("LogisticRegression.predict_proba is not implemented yet.")
+        
+    def predict(self, X):
+        # ====================================
+        # YOUR CODE GOES HERE
+        # ====================================
+        raise NotImplementedError("LogisticRegression.predict is not implemented yet.")
+    
+    def sigmoid(self, z):
+        # ====================================
+        # YOUR CODE GOES HERE
+        # ====================================
+        raise NotImplementedError("LogisticRegression.sigmoid is not implemented yet.")
