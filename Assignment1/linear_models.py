0068a87addd620eeedc801454ff7e2248c2fdb0b
Jostein <jostroda@stud.ntnu.no>
2026-09-09 19:15:10 +0200

    start working on logistic regression model

--- Assignment1/linear_models.py
+++ Assignment1/linear_models.py
@@ -50,12 +50,6 @@ class LinearRegression():
             self.loss_history.append(mse)
 
         return self
-
-
-
-
-
-
         #raise NotImplementedError("LinearRegression.fit is not implemented yet.")
     
     def predict(self, X):
@@ -82,6 +76,7 @@ class LinearRegression():
         # LOGISTIC REGRESSION
     
 class LogisticRegression():
+
     def __init__(self, lr=0.001, n_iterations=1000):
         self.weights = None
         self.bias = None
@@ -96,15 +91,37 @@ class LogisticRegression():
         # YOUR CODE GOES HERE
         # ====================================
 
+        m = X.shape[1] # Total observasjoner
+        n = X.shape[0] # features
 
-        self.weights = np.zeros(X.shape[1]) # X.shape = Datapunkter
+        self.weights = np.zeros((n,1)) # Datapunkter
         self.bias = 0
+
+        cost_list = []
 
 
         # Gradient Descent
+        for i in range(self.n_iterations):
 
-        for _ in range(self.n_iterations):
-            y_pred = np.dot(X, self.weights) + self.bias # Formel for y-pred
+            z = np.dot(self.weights.T, X) + self.bias
+            A = self.sigmoid(z)
+
+            cost = -(1/m)*np.sum( y*np.log(A) + (1-y)*np.log(1-A))
+
+            dW = (1/m)*np.dot(A-y, X.T)
+            dB = (1/m)*np.sum(A - y)
+
+            self.weights = self.weights - self.lr * dW.T
+            self.bias = self.bias - self.lr * dB
+
+            cost_list.append(cost)
+
+
+
+        return self.weights, self.bias, cost_list
+
+    """  
+            y_pred = self.predict(X) # Formel for y-pred
 
             m = X.shape[0] # Antall elementer
 
@@ -124,22 +141,32 @@ class LogisticRegression():
             self.loss_history.append(mse)
 
 
-        return self 
-            
+        
+"""
+
+
+
 
 
         # raise NotImplementedError("LogisticRegression.fit is not implemented yet.")
     
+    # Gir selve modellen sin rå output. En sannsynlighet for at hver rad i X hører til den positive klassen
     def predict_proba(self, X):
         # ====================================
         # YOUR CODE GOES HERE
         # ====================================
+        z = X*self.weights + self.bias
+
+        return self.sigmoid(z)
+    
 
         
 
-        raise NotImplementedError("LogisticRegression.predict_proba is not implemented yet.")
-        
+        # raise NotImplementedError("LogisticRegression.predict_proba is not implemented yet.")
+
+    # Den endelige diskre besluttningen - 0 eller 1, ja eller nei
     def predict(self, X):
+        
         # ====================================
         # YOUR CODE GOES HERE
         # ====================================
@@ -147,7 +174,7 @@ class LogisticRegression():
         lin_model = np.matmul(X, self.weights) + self.bias
         y_pred = self._sigmoid(lin_model)
         return [1 if _y > 0.5 else 0 for _y in y_pred]
-        #
+        
         # raise NotImplementedError("LogisticRegression.predict is not implemented yet.")
     
     def sigmoid(self, z):
